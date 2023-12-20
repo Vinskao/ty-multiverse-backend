0d873f0e5191d5020b455a0a7a30d1b47d670837
Vinskao <tianyikao@gmail.com>
2023-12-20 09:03:31 +0800

    changes

--- src/main/java/tw/com/ty/controller/LoginAjaxController.java
+++ src/main/java/tw/com/ty/controller/LoginAjaxController.java
@@ -1,18 +1,16 @@
 package tw.com.ty.controller;
 
 import java.util.HashMap;
-import java.util.Locale;
 import java.util.Map;
 
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
-import org.springframework.context.i18n.LocaleContextHolder;  // Import this class
+import org.springframework.context.i18n.LocaleContextHolder; 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import tw.com.ty.domain.CustomerBean;
@@ -29,23 +27,20 @@ public class LoginAjaxController {
     @Autowired
     private CustomerService customerService;
 
-    @PostMapping(path = {"/secure/login.controller"})
-    public String login(@RequestBody String json) {
+    @PostMapping(path = {"/ajex.controller"})
+    public String login(@RequestBody String data) {
         JSONObject resJson = new JSONObject();
+		JSONObject jsonList = new JSONObject(data);
 
         Map<String, String> errorMsgs = new HashMap<>();
-		JSONObject jsonList = new JSONObject(json);
         if (jsonList.getString("username") == null || jsonList.getString("username").length() == 0) {
-            errorMsgs.put("xxx1", messageSource.getMessage("login.required.id", null, LocaleContextHolder.getLocale()));
+            errorMsgs.put("username", "請輸入帳號");
         }
         if (jsonList.getString("password") == null || jsonList.getString("password").length() == 0) {
-            errorMsgs.put("xxx2", messageSource.getMessage("login.required.pwd", null, LocaleContextHolder.getLocale()));
+            errorMsgs.put("password", "請輸入密碼");
         }
-
-        // Add error messages to the response JSON
         resJson.put("errors", errorMsgs);
 
-        // If there are errors, return the JSON response with error messages
         if (!errorMsgs.isEmpty()) {
             resJson.put("message", "validation_failed");
             resJson.put("success", false);
@@ -57,11 +52,11 @@ public class LoginAjaxController {
 
         // 根據執行結果呼叫View
         if (bean == null) {
-            resJson.put("message", "failed");
+            resJson.put("message", "無此帳密");
             resJson.put("success", false);
             return resJson.toString();
         } else {
-            resJson.put("message", "success");
+            resJson.put("message", "成功登入");
             resJson.put("success", true);
             return resJson.toString();
         }
