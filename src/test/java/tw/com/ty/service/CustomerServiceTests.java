0f04ca9e7135a5e290e5fac3ea697ee4b9acdbfc
Vinskao <tianyikao@gmail.com>
2023-12-25 08:58:45 +0800

    misc

--- src/test/java/tw/com/ty/service/CustomerServiceTests.java
+++ src/test/java/tw/com/ty/service/CustomerServiceTests.java
@@ -1,25 +1,25 @@
-package tw.com.ty.service;
+// package tw.com.ty.service;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.boot.test.context.SpringBootTest;
 
-import tw.com.ty.domain.CustomerBean;
+// import tw.com.ty.domain.CustomerBean;
 
-@SpringBootTest
-public class CustomerServiceTests {
-	@Autowired
-	private CustomerService customerService;
+// @SpringBootTest
+// public class CustomerServiceTests {
+// 	@Autowired
+// 	private CustomerService customerService;
 	
-	@Test
-	public void testLogin() {
-		CustomerBean login = customerService.login(null, null);
-		System.out.println("login="+login);		
-	}
+// 	@Test
+// 	public void testLogin() {
+// 		CustomerBean login = customerService.login(null, null);
+// 		System.out.println("login="+login);		
+// 	}
 	
-	@Test
-	public void testChangePassword() {
-		boolean change = customerService.changePassword("Ellen", "EEE", "E");
-		System.out.println("change="+change);
-	}
-}
+// 	@Test
+// 	public void testChangePassword() {
+// 		boolean change = customerService.changePassword("Ellen", "EEE", "E");
+// 		System.out.println("change="+change);
+// 	}
+// }
