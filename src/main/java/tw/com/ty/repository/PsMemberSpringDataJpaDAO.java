a00e305ec8811c609d19c16f46d11b658c05b283
Vinskao <tianyikao@gmail.com>
2024-01-03 21:16:17 +0800

    處理錯誤覆蓋

--- src/main/java/tw/com/ty/repository/PsMemberSpringDataJpaDAO.java
+++ src/main/java/tw/com/ty/repository/PsMemberSpringDataJpaDAO.java
@@ -4,11 +4,11 @@ import java.util.List;
 
 import org.json.JSONObject;
 
-import tw.com.ty.domain.ProductBean;
+import tw.com.ty.domain.PsMemberBean;
 //客製化功能放此
 public interface PsMemberSpringDataJpaDAO {
     //spring會自動加上abstract
-    public abstract List<ProductBean> find(JSONObject param);
+    public abstract List<PsMemberBean> find(JSONObject param);
     public abstract long count(JSONObject param);
 
 }
