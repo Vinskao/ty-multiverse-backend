1d68513ee54c4ec3cad76e5a98afe2f5de588c17
Vinskao <tianyikao@gmail.com>
2025-02-01 23:38:40 +0800

    feat: acuator metrics ws done

--- src/main/java/tw/com/tymbackend/TYMBackendApplication.java
+++ src/main/java/tw/com/tymbackend/TYMBackendApplication.java
@@ -4,26 +4,24 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import tw.com.tymbackend.config.WebSocketMetricsExporter;
-import org.springframework.core.env.Environment;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 @SpringBootApplication
+@EnableWebSocket
 public class TYMBackendApplication {
 	private static final Logger logger = LoggerFactory.getLogger(TYMBackendApplication.class);
-
 	public static void main(String[] args) {
-		ApplicationContext context = SpringApplication.run(TYMBackendApplication.class, args);
-		
-		// Get the environment to check configurations
-		Environment env = context.getEnvironment();
-		String managementBasePath = env.getProperty("management.endpoints.web.base-path");
-		
-		logger.info("Management Base Path: " + managementBasePath);
-		
-		// 確認 WebSocketMetricsExporter 已被創建並運行
-		WebSocketMetricsExporter exporter = context.getBean(WebSocketMetricsExporter.class);
-		System.err.println("exporter" + exporter);
-		logger.info("WebSocketMetricsExporter 已成功運行");
+		SpringApplication.run(TYMBackendApplication.class, args);
 	}
+    /**
+     * 自動註冊使用了@ServerEndpoint註解聲明的Websocket endpoint      
+     * 注意：如果使用獨立的servlet容器，而不是直接使用springboot的內置容器
+     * 請不要注入ServerEndpointExporter，因為它將由容器自己提供和管理。    
+     */
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
+    }
 }
