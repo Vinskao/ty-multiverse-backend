845fe32437d9b9776aa4474fc6a6834c3754266f
Vinskao <tianyikao@gmail.com>
2025-04-10 19:39:24 +0800

    info: resolving logout

--- weapon-data.sql
+++ weapon-data.sql
@@ -5,6 +5,9 @@ INSERT INTO weapon(name, weapon, attributes, base_damage, bonus_damage, bonus_at
 VALUES ('hitomi', '噬人', '波', 1399, 399, ARRAY['波','光'], ARRAY['擊退']);
 
 INSERT INTO weapon(name, weapon, attributes, base_damage, bonus_damage, bonus_attributes, state_attributes)
-VALUES ('chiaki', 'Lord of Anal Beads ', '淫', 142, 1840, ARRAY['淫','氣'], ARRAY['無力']);
+VALUES ('chiaki', 'Lord of Anal Beads', '淫', 142, 1840, ARRAY['淫','氣'], ARRAY['無力']);
+
+INSERT INTO weapon(name, weapon, attributes, base_damage, bonus_damage, bonus_attributes, state_attributes)
+VALUES ('samui', '冰の女王', '冰', 142, 1840, ARRAY['冰'], ARRAY['冷凍']);
 
 
